5193e42986dca178a89ae0b49fffbaf01f98ebb3
Lukas Bischof <lukas.bischof@renuo.ch>
2018-08-14 18:27:37 +0200

    Shader class

---
+++ engine/Shader.js
@@ -0,0 +1,41 @@
+(function() {
+  "use strict";
+
+  function getShader(gl, id){
+    var shaderScript = document.getElementById(id);
+    if (!gl || !id || !shaderScript)
+      return null;
+
+    var str = '';
+    var k = shaderScript.firstChild;
+    while (k) {
+      if (k.nodeType === 3)
+        str += k.textContent;
+
+      k = k.nextSibling;
+    }
+
+    var shader;
+    if (shaderScript.type === "x-shader/x-fragment")
+      shader = gl.createShader(gl.FRAGMENT_SHADER);
+    else if (shaderScript.type === "x-shader/x-vertex")
+      shader = gl.createShader(gl.VERTEX_SHADER);
+    else
+      return null;
+
+    gl.shaderSource(shader, str);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      alert(gl.getShaderInfoLog(shader));
+      gl.deleteShader(shader);
+      return null;
+    }
+
+    return shader;
+  }
+
+  window.Shader = function(gl, id) {
+    this.shader = getShader(gl, id);
+  };
+})();
