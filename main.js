b45e7c2554e0f426b9ff2d7ea03b94bf9820a7ff
Lukas Bischof <lukas.bischof@renuo.ch>
2018-08-16 18:13:14 +0200

    Support viewport change

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 (function() {
   "use strict";
 
-  var game;
+  var game, canvas, renderer;
   window.addEventListener('load', function() {
-    var renderer = new Renderer(document.getElementById('the-game'));
+    canvas = document.getElementById('the-game');
+    renderer = new Renderer(canvas);
     renderer.initialize();
     game = new Game(renderer);
+    resizeCanvas();
     mainLoop();
   });
+
+  window.addEventListener('resize', resizeCanvas);
+
+  function resizeCanvas() {
+    var devicePixelRatio = window.devicePixelRatio || 1;
+
+    canvas.width = window.innerWidth * devicePixelRatio;
+    canvas.height = window.innerHeight * devicePixelRatio;
+    renderer.resizeViewport();
+  }
 
   function mainLoop() {
     game.loop();
