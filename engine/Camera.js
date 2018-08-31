0a089250e73ce4c54cdcd4e16ab40e76f21b519e
Lukas Bischof <lukas.bischof@renuo.ch>
2018-08-31 14:45:52 +0200

    Delete Cube and new Camera

--- engine/Camera.js
+++ engine/Camera.js
@@ -1,6 +1,7 @@
 class Camera {
   constructor() {
-    this.transformationMatrix = GLMath.matrix4Identity();
+    this.position = [0, 0, 0];
+    this.rotation = [0, 0, 0];
   }
 
   moveX(amount) {
@@ -15,7 +16,18 @@ class Camera {
     this.translate([0, 0, amount]);
   }
 
+  rotateX(amount) {
+    this.rotation = [this.rotation[0] + amount, 0, 0];
+  }
+
   translate(vector) {
-    this.transformationMatrix = GLMath.matrix4Translate(this.transformationMatrix, vector.map(i => -i));
+    this.position = this.position.map((val, i) => val + vector[i]);
+  }
+
+  get transformationMatrix() {
+    let matrix = GLMath.matrix4Identity();
+    matrix = GLMath.matrix4Rotate(matrix, this.rotation[0], [1, 0, 0]);
+    matrix = GLMath.matrix4Translate(matrix, [-this.position[0], -this.position[1], -this.position[2]]);
+    return matrix;
   }
 }
