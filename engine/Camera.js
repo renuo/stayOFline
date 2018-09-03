71833b11379f6caa030a8ca11b8e0c57132ae0b1
Josua Schmid <josua.schmid@renuo.ch>
2018-09-03 21:05:12 +0200

    Add more rotation code

--- engine/Camera.js
+++ engine/Camera.js
@@ -16,8 +16,17 @@ class Camera {
     this.translate([0, 0, amount]);
   }
 
+  // TODO: Remove all rotation code in the final version
   rotateX(amount) {
     this.rotation = [this.rotation[0] + amount, 0, 0];
+  }
+
+  rotateY(amount) {
+    this.rotation = [0, this.rotation[0] + amount, 0];
+  }
+
+  rotateZ(amount) {
+    this.rotation = [0, 0, this.rotation[0] + amount];
   }
 
   translate(vector) {
@@ -26,8 +35,10 @@ class Camera {
 
   get transformationMatrix() {
     let matrix = GLMath.matrix4Identity();
-    matrix = GLMath.matrix4Rotate(matrix, this.rotation[0], [1, 0, 0]);
     matrix = GLMath.matrix4Translate(matrix, [this.position[0], this.position[1], this.position[2]]);
+    matrix = GLMath.matrix4Rotate(matrix, this.rotation[0], [1, 0, 0]);
+    matrix = GLMath.matrix4Rotate(matrix, this.rotation[1], [0, 1, 0]);
+    matrix = GLMath.matrix4Rotate(matrix, this.rotation[2], [0, 0, 1]);
     return GLMath.matrix4Inverse(matrix);
   }
 }
